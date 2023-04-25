9bc65708ab9b0da35bf255b7717c8dc886e7aaa6
Dunhill Dale Quiachon <dunhillq@codev.com>
2023-04-25 22:32:44 +0800

    Changes in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,8 +17,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $types = ['admin', 'hr', 'employee']; 
+        $type = array_rand($types, 1);
+
         return [
-            'name' => fake()->name(),
+            'employee_id' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'middle_name' => fake()->name(),
+            'last_name' => fake()->lastName(),
+            'type' => $types[$type],
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
