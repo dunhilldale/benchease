18894f41a9af1a4b027c16a7a616c9fc3dedabd3
Dunhill Dale Quiachon <dunhillq@codev.com>
2023-05-05 12:08:25 +0800

    Added phone1 and phone2 sample data in User migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 return new class extends Migration
@@ -38,7 +39,9 @@ return new class extends Migration
         User::create([
             'employee_id' => '2022-001',
             'first_name' => 'Admin',
-            'last_name' => ' Admin',
+            'last_name' => 'Admin',
+            'phone_1' => '+63 921 123 5467',
+            'phone_2' => '+63 921 890 1234',
             'type' => 'admin',
             'is_new' => false,
             'email' => 'admin@sample.com',
@@ -47,7 +50,9 @@ return new class extends Migration
         User::create([
             'employee_id' => '2022-002',
             'first_name' => 'Sample',
-            'last_name' => ' HR',
+            'last_name' => 'HR',
+            'phone_1' => '+63 921 123 5467',
+            'phone_2' => '+63 921 890 1234',
             'type' => 'hr',
             'is_new' => false,
             'email' => 'hr@sample.com',
@@ -56,7 +61,9 @@ return new class extends Migration
         User::create([
             'employee_id' => '2022-003',
             'first_name' => 'Sample',
-            'last_name' => ' Employee',
+            'last_name' => 'Employee',
+            'phone_1' => '+63 921 123 5467',
+            'phone_2' => '+63 921 890 1234',
             'type' => 'employee',
             'is_new' => true,
             'email' => 'employee@sample.com',
